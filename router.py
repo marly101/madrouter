6d8c2b5abbe082265458a8f1886f4ba5f5e3084f
iri <irigur100@gmail.com>
2026-03-12 13:37:19 +0200

    changed router.py to use a route table and add functionality

--- router.py
+++ router.py
@@ -1,17 +1,35 @@
 import scapy.all as scapy
-import socket
-import select
 
-RECV_IFACE = "enp0s8"
-SEND_IFACE = "enp0s9"
-RECV_SIZE = 1500
-ETH_P_ALL = 3
-ETH_P_IP = 0x800
+IFACE_1 = "enp0s8"
+IFACE_2 = "enp0s9"
+ICMP_UNREACHABLE_TYPE = 3
+ICMP_HOST_UNREACHABLE_CODE = 1
+ICMP_TIME_EXCEEDED_TYPE = 11
+ICMP_TTL_EXPIRED_CODE = 0
+LOOPBACK = "lo"
 
-s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
-s.bind((RECV_IFACE, 0))
-sock_select = select.select([s], [], [])
-while True:
-    for sock in sock_select[0]:
-        recieved = sock.recv(RECV_SIZE)
-        scapy.sendp(recieved, iface=SEND_IFACE)
+def route_packet(packet):
+    if not packet.haslayer(scapy.IP):
+        return
+    packet[scapy.IP].ttl -= 1
+    if packet[scapy.IP].ttl <= 0:
+        scapy.send(scapy.IP(dst=packet[scapy.IP].src) 
+                    / scapy.ICMP(type=ICMP_TIME_EXCEEDED_TYPE, code=ICMP_TTL_EXPIRED_CODE))
+        return
+    to_send_iface = scapy.conf.route.route(packet[scapy.IP].dst)[0]
+    # Send ICMP unreachable if dst IP isn't in routing table
+    if to_send_iface == LOOPBACK:
+        print(scapy.IP(dst=packet[scapy.IP].src) 
+                    / scapy.ICMP(type=ICMP_UNREACHABLE_TYPE, code=ICMP_HOST_UNREACHABLE_CODE))
+        scapy.send(scapy.IP(dst=packet[scapy.IP].src) 
+                    / scapy.ICMP(type=ICMP_UNREACHABLE_TYPE, code=ICMP_HOST_UNREACHABLE_CODE))
+        return
+    print(scapy.Ether() / packet[scapy.IP])
+    print(to_send_iface)
+    scapy.sendp(scapy.Ether() / packet[scapy.IP], iface=to_send_iface, verbose=True)
+
+def main():
+    scapy.sniff(iface=[IFACE_1, IFACE_2, "enp0s3"], prn=route_packet, store=False, filter="inbound")
+
+if __name__ == "__main__":
+    main()
